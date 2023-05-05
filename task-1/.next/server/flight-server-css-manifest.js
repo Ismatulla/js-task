df1547f79805d9b627af838a878ef6c5c74f16af
Ismatulla <ismatilloqoyliyev@gmail.com>
2023-05-05 16:19:29 +0200

    init-project

---
+++ task-1/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "C:\\Users\\dell\\OneDrive\\Desktop\\fulfil-edu\\js-task\\task-1\\src\\app\\page": [
+      "C:\\Users\\dell\\OneDrive\\Desktop\\fulfil-edu\\js-task\\task-1\\src\\app\\page.module.css",
+      "C:\\Users\\dell\\OneDrive\\Desktop\\fulfil-edu\\js-task\\task-1\\src\\app\\globals.css"
+    ]
+  },
+  "C:\\Users\\dell\\OneDrive\\Desktop\\fulfil-edu\\js-task\\task-1\\src\\app\\layout.tsx": [
+    "C:\\Users\\dell\\OneDrive\\Desktop\\fulfil-edu\\js-task\\task-1\\src\\app\\globals.css"
+  ],
+  "C:\\Users\\dell\\OneDrive\\Desktop\\fulfil-edu\\js-task\\task-1\\src\\app\\page.tsx": [
+    "C:\\Users\\dell\\OneDrive\\Desktop\\fulfil-edu\\js-task\\task-1\\src\\app\\page.module.css"
+  ]
+}
